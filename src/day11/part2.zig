40753cb874a6b409e90dc0690528f09b33868910
Beizbol <30479707+Beizbol@users.noreply.github.com>
2024-12-18 01:50:48 -0500

    save

--- src/day11/part2.zig
+++ src/day11/part2.zig
@@ -40,122 +40,170 @@ test "digits" {
     std.testing.expect(n == 4) catch std.debug.print("ans: 4 | got: {d}\n", .{n});
 }
 
-const Val = struct {
+// const Val = struct {
+//     left: usize,
+//     right: usize,
+//     count: usize,
+// };
+
+const Pair = struct {
     left: usize,
     right: usize,
     count: usize,
 };
-const Set = std.AutoHashMap(usize, Val);
+
+const Solo = struct {
+    v: usize,
+    count: usize,
+};
+
+const PSet = std.AutoHashMap(usize, Pair);
+const SSet = std.AutoHashMap(usize, Solo);
 const List = std.ArrayList(usize);
 
-const Iter = struct {};
-
 const Ctx = struct {
-    iters: [75]Set,
-    set: Set,
-    list: List,
+    input: List,
+    pair_set: PSet,
+    solo_set: SSet,
+    iter: usize,
+    pairs: [76]PSet,
+    solos: [76]SSet,
 
     pub fn init(alloc: std.mem.Allocator) !Ctx {
         return .{
-            .iters = .{Set.init(alloc)} ** 75,
-            .set = Set.init(alloc),
-            .list = List.init(alloc),
+            .input = List.init(alloc),
+            .pairs = .{PSet.init(alloc)} ** 76,
+            .solos = .{SSet.init(alloc)} ** 76,
+            .pair_set = PSet.init(alloc),
+            .solo_set = SSet.init(alloc),
+            .iter = 0,
         };
     }
 
     pub fn add_stone(ctx: *Ctx, stone: []const u8) !void {
         const n = try std.fmt.parseInt(usize, stone, 10);
-        try ctx.list.append(n);
+        try ctx.input.append(n);
     }
 
-    pub fn stones(ctx: Ctx) usize {
-        return ctx.list.items.len;
+    // pub fn stones(ctx: Ctx) usize {
+
+    //     const set_count = ctx.sets[ctx.iter].count() * 2;
+    //     const list_count = ctx.lists[ctx.iter].items.len;
+    //     return set_count + list_count;
+    // }
+
+    pub fn handle_item(ctx: *Ctx, item: usize) !void {
+        if (ctx.solos[ctx.iter].getPtr(item)) |v| {
+            v.count += 1;
+            return;
+        }
+        if (ctx.pairs[ctx.iter].getPtr(item)) |v| {
+            v.count += 1;
+            return;
+        }
+        if (ctx.solo_set.get(item)) |v| {
+            try ctx.solos[ctx.iter].put(item, v);
+            return;
+        }
+        if (ctx.pair_set.get(item)) |v| {
+            try ctx.pairs[ctx.iter].put(item, v);
+            return;
+        }
+        const count = try digit_count(item);
+        if (count % 2 == 1) {
+            try ctx.solos[ctx.iter].put(item, .{ .v = item * 2024, .count = 1 });
+            try ctx.solo_set.put(item * 2024, .{ .v = item * 2024, .count = 1 });
+            return;
+        }
+
+        const pow = std.math.pow(usize, 10, count / 2);
+        const left = item / pow;
+        const right = item - (left * pow);
+        const v = Pair{ .left = left, .right = right, .count = 1 };
+        try ctx.pairs[ctx.iter].put(item, v);
+        try ctx.pair_set.put(item, v);
     }
 
-    pub fn expand_fast(ctx: *Ctx, iter: usize) !void {
-        const len = ctx.list.items.len;
-        for (0..len) |_i| {
-            const i = len - (_i + 1);
-            const item = ctx.list.items[i];
-            // std.debug.print("i:{d},item:{d}\n", .{ i, item });
-            if (item == 0) {
-                ctx.list.items[i] = 1;
-                continue;
-            }
-            if (ctx.iters[iter].getPtr(item)) |v| {
-                v.count += 1;
-                continue;
-            }
-            if (ctx.set.get(item)) |v| {
-                try ctx.iters[iter].put(item, v);
-                // ctx.list.items[i] = v.left;
-                // try ctx.list.append(v.right);
-                continue;
-            }
-            const count = try digit_count(item);
-            if (count % 2 == 1) {
-                ctx.list.items[i] = item * 2024;
-                continue;
-            }
+    pub fn expand_fast(ctx: *Ctx) !void {
+        ctx.iter += 1;
+        const iter = ctx.iter - 1;
 
-            const pow = std.math.pow(usize, 10, count / 2);
-            const left = item / pow;
-            const right = item - (left * pow);
-            // ctx.list.items[i] = left;
-            // try ctx.list.append(right);
-            const v = Val{ .left = left, .right = right, .count = 1 };
-            try ctx.iters[iter].put(item, v);
-            try ctx.set.put(item, v);
+        if (iter == 0) {
+            for (ctx.input.items) |item| {
+                try ctx.handle_item(item);
+            }
+            return;
+        }
+
+        // Singles
+        var solos = ctx.solos[iter].iterator();
+        while (solos.next()) |entry| {
+            try ctx.handle_item(entry.key_ptr.*);
+        }
+
+        // Pairs
+        var pairs = ctx.pairs[iter].iterator();
+        while (pairs.next()) |entry| {
+            try ctx.handle_item(entry.value_ptr.left);
+            try ctx.handle_item(entry.value_ptr.right);
         }
     }
 
-    pub fn tally_stats(ctx: Ctx, iter: usize) usize {
-        std.debug.print("tally ({d})\n", .{iter});
+    pub fn tally_stats(ctx: Ctx) usize {
+        std.debug.print("tally ({d})\n", .{ctx.iter});
         var total: usize = 0;
-        for (0..iter + 1) |_i| {
-            const i = iter - _i;
+
+        for (0..ctx.iter + 1) |_i| {
+            const i = ctx.iter - _i;
             // std.debug.print("i:{d},iter:{d},_i:{d}\n", .{ i, iter, _i });
-            var set_iter = ctx.iters[i].iterator();
-            while (set_iter.next()) |pair| {
-                // std.debug.print("k:{d} v:{any}\n", .{ pair.key_ptr.*, pair.value_ptr.* });
-
-                if (i == iter) {
-                    pair.value_ptr.count = 2;
-                    if (iter == 0) total += 2;
-                    // std.debug.print("+2\n", .{});
-                    continue;
-                }
+            var pair_iter = ctx.pairs[i].iterator();
+            while (pair_iter.next()) |pair| {
                 const L = pair.value_ptr.left;
-                const R = pair.value_ptr.right;
-
-                // const prev =
-
-                // std.debug.print("i+1:\n", .{});
-                // var next_iter = ctx.iters[i + 1].iterator();
-                // while (next_iter.next()) |entry| {
-                //     std.debug.print("k:{d} v:{any}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
-                // }
-                // std.debug.print("i+1.\n", .{});
-
                 var NL: usize = 1;
-                if (ctx.iters[i + 1].get(L)) |val| {
+                if (i == ctx.iter) {
+                    NL = 1;
+                } else if (ctx.pairs[i + 1].get(L)) |val| {
+                    NL = val.count;
+                } else if (ctx.solos[i + 1].get(L)) |val| {
                     NL = val.count;
                 }
+
+                const R = pair.value_ptr.right;
                 var NR: usize = 1;
-                if (ctx.iters[i + 1].get(R)) |val| {
+                if (i == ctx.iter) {
+                    NR = 1;
+                } else if (ctx.pairs[i + 1].get(R)) |val| {
                     NR = val.count;
+                } else if (ctx.solos[i + 1].get(L)) |val| {
+                    NL = val.count;
                 }
 
-                // std.debug.print("nl: {d} | nr: {d}\n", .{ NL, NR });
                 pair.value_ptr.count *= NL + NR;
                 if (i == 0) {
                     total += pair.value_ptr.count;
                 }
             }
+
+            var solo_iter = ctx.solos[i].iterator();
+            while (solo_iter.next()) |solo| {
+                // std.debug.print("k:{d} v:{any}\n", .{ pair.key_ptr.*, pair.value_ptr.* });
+                const V = solo.value_ptr.v;
+                var NV: usize = 1;
+                if (i == ctx.iter) {
+                    NV = 1;
+                } else if (ctx.pairs[i + 1].get(V)) |val| {
+                    NV = val.count;
+                } else if (ctx.solos[i + 1].get(V)) |val| {
+                    NV = val.count;
+                }
+
+                solo.value_ptr.count *= NV;
+                if (i == 0) {
+                    total += solo.value_ptr.count;
+                }
+            }
             // std.debug.print("i:{d},iter:{d},_i:{d}\n", .{ i, iter, _i });
         }
-
-        total += ctx.list.items.len;
 
         return total;
     }
@@ -179,10 +227,10 @@ pub fn sln() !void {
         try ctx.add_stone(stone);
     }
     // Expand
-    for (0..75) |i| {
+    for (0..75) |_| {
         // ctx.iters[i] = Set.init(alloc);
-        try ctx.expand_fast(i);
-        std.debug.print("{d}:  stones={d}  sets={d}\n", .{ i + 1, ctx.stones(), ctx.set.count() });
+        try ctx.expand_fast();
+        // std.debug.print("{d}:  stones={d}  sets={d}\n", .{ i + 1, ctx.stones(), ctx.set.count() });
     }
     // Tally
     const result = ctx.tally_stats();
@@ -198,27 +246,29 @@ test "expand fast" {
     var ctx = try Ctx.init(alloc);
     try ctx.add_stone("125");
     try ctx.add_stone("17");
-    try ctx.expand_fast(0);
-    var n = ctx.tally_stats(0);
+
+    try ctx.expand_fast();
+    var n = ctx.tally_stats();
     std.testing.expect(n == 3) catch std.debug.print("got: {d}\n", .{n});
 
-    try ctx.expand_fast(1);
-    n = ctx.tally_stats(1);
+    try ctx.expand_fast();
+    n = ctx.tally_stats();
     std.testing.expect(n == 4) catch std.debug.print("got: {d}\n", .{n});
 
-    try ctx.expand_fast(2);
-    n = ctx.tally_stats(2);
+    try ctx.expand_fast();
+    n = ctx.tally_stats();
     std.testing.expect(n == 5) catch std.debug.print("got: {d}\n", .{n});
 
-    try ctx.expand_fast(3);
-    n = ctx.tally_stats(3);
+    try ctx.expand_fast();
+    n = ctx.tally_stats();
     std.testing.expect(n == 9) catch std.debug.print("got: {d}\n", .{n});
 
-    try ctx.expand_fast(4);
-    n = ctx.tally_stats(4);
+    try ctx.expand_fast();
+    n = ctx.tally_stats();
     std.testing.expect(n == 13) catch std.debug.print("got: {d}\n", .{n});
 
-    try ctx.expand_fast(5);
-    n = ctx.tally_stats(5);
+    try ctx.expand_fast();
+    n = ctx.tally_stats();
     std.testing.expect(n == 22) catch std.debug.print("got: {d}\n", .{n});
+    try std.testing.expect(n == 22);
 }
